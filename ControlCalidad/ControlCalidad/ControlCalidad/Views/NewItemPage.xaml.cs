cad3be3bab7f7e547261b2940bbb24108baa10ed
Federico Gabriel Fia Sare <fedefiasare@hotmail.com>
2020-09-06 21:08:27 -0300

    Initial Commit

---
+++ ControlCalidad/ControlCalidad/ControlCalidad/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using ControlCalidad.Models;
+using ControlCalidad.ViewModels;
+
+namespace ControlCalidad.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
