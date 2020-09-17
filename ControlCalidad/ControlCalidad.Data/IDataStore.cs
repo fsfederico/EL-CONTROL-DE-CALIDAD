d149a6435d40017d84d0f62b15579fcab7892c5b
Federico Gabriel Fia Sare <fedefiasare@hotmail.com>
2020-09-17 02:35:29 -0300

    Mock database

---
+++ ControlCalidad/ControlCalidad.Data/IDataStore.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ControlCalidad.Datos
+{
+    public interface IDataStore<T>
+    {
+        Task<bool> AddItemAsync(T item);
+        Task<bool> UpdateItemAsync(T item);
+        Task<bool> DeleteItemAsync(string id);
+        Task<T> GetItemAsync(int id);
+        Task<IEnumerable<T>> GetItemsAsync(bool forceRefresh = false);
+    }
+}
---
+++ ControlCalidad/ControlCalidad.Data/MockDataStore.cs
@@ -0,0 +1,46 @@
+﻿using ControlCalidad.Dominio.Entidades;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ControlCalidad.Datos
+{
+    public static class MockDataStore
+    {
+        public static List<Usuario> Usuarios = new List<Usuario>() {
+            new Usuario{
+                Id = 1,
+                Apellido= "admin",
+                Nombre = "admin",
+                CodigoUsuario = "admin",
+                Email = "admin",
+                Documento = "1",
+                Rol = Dominio.Rol.Administrador
+            }
+        };
+
+        public static List<Color> Colores = new List<Color>{
+            new Color { Id = 1, Descripcion="Rojo" },
+            new Color { Id = 2, Descripcion="Negro" },
+            new Color { Id = 3, Descripcion="Blanco" }
+        };
+
+        public static IEnumerable<Color> ColoresActivos => Colores.Where(c => c.Estado == Dominio.Enums.Estado.Activo);
+
+        public static int ColorNextId => Colores.Count < 0 ? 1 : Colores.Max(c => c.Id) + 1;
+
+        public static void AddOrUpdate<T>(this List<T> list, T item) where T : EntityBase
+        {
+            var index = list.FindIndex(l => l.Id == item.Id);
+
+            if (index >= 0)
+            {
+                list[index] = item;
+            }
+            else
+            {
+                item.Id = list.Count() > 0 ? list.Max(c => c.Id) + 1 : 1;
+                list.Add(item);
+            }
+        }
+    }
+}
