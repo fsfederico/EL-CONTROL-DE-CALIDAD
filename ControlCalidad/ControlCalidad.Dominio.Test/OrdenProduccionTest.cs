66617eeddbe3409685b1fefd8fc715deb7ca9164
Federico Gabriel Fia Sare <fedefiasare@gmail.com>
2020-12-01 23:46:00 -0300

    orden produccion test

--- ControlCalidad/ControlCalidad.Dominio.Test/OrdenProduccionTest.cs
+++ ControlCalidad/ControlCalidad.Dominio.Test/OrdenProduccionTest.cs
@@ -1,10 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using NUnit.Framework;
 
 namespace ControlCalidad.Dominio.Test
 {
-    class OrdenProduccionTest
+    public class OrdenProduccionTest
     {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Prueba_Continuar_Orden_Produccion_Pausada()
+        {
+            //Etapa de Preparacion
+            var ordenProduccion = new OrdenProduccion
+            {
+                EstadoOrdenProduccion = Enums.EstadoOrdenProduccion.Pausado
+            };
+            //Etapa de Ejecucion
+            ordenProduccion.Continuar();
+            //Etapa de Comprobacion
+            Assert.AreEqual(ordenProduccion.EstadoOrdenProduccion, Enums.EstadoOrdenProduccion.EnProgreso);
+        }
+
+        [Test]
+        public void Prueba_Continuar_Orden_Produccion_Finalizada()
+        {
+            //Etapa de Preparacion
+            var ordenProduccion = new OrdenProduccion
+            {
+                EstadoOrdenProduccion = Enums.EstadoOrdenProduccion.Finalizado
+            };
+            //Etapa de Ejecucion
+            ordenProduccion.Continuar();
+            //Etapa de Comprobacion
+            Assert.AreEqual(ordenProduccion.EstadoOrdenProduccion, Enums.EstadoOrdenProduccion.Finalizado);
+        }
     }
 }
