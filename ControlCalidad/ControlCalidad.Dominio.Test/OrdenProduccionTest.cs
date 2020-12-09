bb601812e1a4b5a55797f78d8fd08b48678ed246
Federico Gabriel Fia Sare <fedefiasare@gmail.com>
2020-12-09 17:50:49 -0300

    Test Orden de producción disponibilidad para inspeccion

--- ControlCalidad/ControlCalidad.Dominio.Test/OrdenProduccionTest.cs
+++ ControlCalidad/ControlCalidad.Dominio.Test/OrdenProduccionTest.cs
@@ -52,7 +52,7 @@ namespace ControlCalidad.Dominio.Test
         }
 
         [Test]
-        public void Prueba_Orden_Produccion_Sin_Ejecucion()
+        public void Prueba_Orden_Produccion_Finalizada_Sin_Ejecucion()
         {
             //Etapa de Preparacion
             var ordenProduccion = new OrdenProduccion
@@ -89,6 +89,69 @@ namespace ControlCalidad.Dominio.Test
             };
             //Etapa de Ejecucion
             var enEjecucion = ordenProduccion.EnEjecucion;
+            //Etapa de Comprobacion
+            Assert.IsTrue(enEjecucion);
+        }
+
+        [Test]
+        public void Prueba_Orden_Produccion_Finalizada_No_Disponible_Para_Iniciar_Inspeccion()
+        {
+            //Etapa de Preparacion
+            var ordenProduccion = new OrdenProduccion
+            {
+                EstadoOrdenProduccion = Enums.EstadoOrdenProduccion.Finalizado
+            };
+
+            //Etapa de Ejecucion
+            var enEjecucion = ordenProduccion.DisponibleInspeccion;
+
+            //Etapa de Comprobacion
+            Assert.IsFalse(enEjecucion);
+        }
+
+        [Test]
+        public void Prueba_Orden_Produccion_Pausada_No_Disponible_Para_Iniciar_Inspeccion()
+        {
+            //Etapa de Preparacion
+            var ordenProduccion = new OrdenProduccion
+            {
+                EstadoOrdenProduccion = Enums.EstadoOrdenProduccion.Pausado
+            };
+
+            //Etapa de Ejecucion
+            var enEjecucion = ordenProduccion.DisponibleInspeccion;
+
+            //Etapa de Comprobacion
+            Assert.IsFalse(enEjecucion);
+        }
+
+        [Test]
+        public void Prueba_Orden_Produccion_En_Progreso_No_Disponible_Para_Iniciar_nspeccion()
+        {
+            //Etapa de Preparacion
+            var ordenProduccion = new OrdenProduccion
+            {
+                EstadoOrdenProduccion = Enums.EstadoOrdenProduccion.EnProgreso
+            };
+            ordenProduccion.InspeccionesOrdenProduccion.Add(new InspeccionOrdenProduccion());
+            //Etapa de Ejecucion
+            var enEjecucion = ordenProduccion.DisponibleInspeccion;
+
+            //Etapa de Comprobacion
+            Assert.IsFalse(enEjecucion);
+        }
+
+        [Test]
+        public void Prueba_Orden_Produccion_En_Progreso_Disponible_Para_Iniciar_nspeccion()
+        {
+            //Etapa de Preparacion
+            var ordenProduccion = new OrdenProduccion
+            {
+                EstadoOrdenProduccion = Enums.EstadoOrdenProduccion.EnProgreso
+            };
+            //Etapa de Ejecucion
+            var enEjecucion = ordenProduccion.DisponibleInspeccion;
+
             //Etapa de Comprobacion
             Assert.IsTrue(enEjecucion);
         }
