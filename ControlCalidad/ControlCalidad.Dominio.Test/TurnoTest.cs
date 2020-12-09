a9fd135d7b624ac99353f65894ccd7f6344a3697
Federico Gabriel Fia Sare <fedefiasare@gmail.com>
2020-12-09 16:46:44 -0300

    Fix test horarios

--- ControlCalidad/ControlCalidad.Dominio.Test/TurnoTest.cs
+++ ControlCalidad/ControlCalidad.Dominio.Test/TurnoTest.cs
@@ -1,6 +1,4 @@
 ﻿using NUnit.Framework;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace ControlCalidad.Dominio.Test
 {
@@ -12,39 +10,33 @@ namespace ControlCalidad.Dominio.Test
         }
 
         [Test]
-        public void Prueba_Puede_Operar()
+        public void Prueba_Horario_Puede_Operar_En_Turno()
         {
             //Etapa de Preparacion
-            var turnos = new List<Turno>()
+            var turno = new Turno
             {
-                new Turno
-                {
-                    Inicio= new System.TimeSpan(18,18,18),
-                    Fin = new System.TimeSpan(18,18,20),
-                    MinutosPermitidos = 120
-                }
+                Inicio = new System.TimeSpan(18, 18, 18),
+                Fin = new System.TimeSpan(18, 18, 20),
+                MinutosPermitidos = 120
             };
             //Etapa de Ejecucion
-            var puedeOperar = turnos.Any(t => t.PuedeOperar(new System.TimeSpan(18, 18, 20)));
+            var puedeOperar = turno.PuedeOperar(new System.TimeSpan(18, 18, 20));
             //Etapa de Comprobacion
             Assert.IsTrue(puedeOperar);
         }
 
         [Test]
-        public void Prueba_No_Puede_Operar()
+        public void Prueba_Horario_No_Puede_Operar_En_Turno()
         {
             //Etapa de Preparacion
-            var turnos = new List<Turno>()
+            var turno = new Turno
             {
-                new Turno
-                {
-                    Inicio= new System.TimeSpan(18,18,18),
-                    Fin = new System.TimeSpan(18,18,20),
-                    MinutosPermitidos = 120
-                }
+                Inicio = new System.TimeSpan(18, 18, 18),
+                Fin = new System.TimeSpan(18, 18, 20),
+                MinutosPermitidos = 120
             };
             //Etapa de Ejecucion
-            var puedeOperar = turnos.Any(t => t.PuedeOperar(new System.TimeSpan(21, 18, 20)));
+            var puedeOperar = turno.PuedeOperar(new System.TimeSpan(21, 18, 20));
             //Etapa de Comprobacion
             Assert.IsFalse(puedeOperar);
         }
